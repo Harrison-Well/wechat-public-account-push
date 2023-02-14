fbec23e4ef36ff9675c087e064d9b8e5ca6b179a
Harrison-Well <62510815+Harrison-Well@users.noreply.github.com>
2023-02-14 12:51:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '睿孜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'obdIU53tTnFyqDIjSUXEFRVZAUPc',
+      id: 'obdIU56eTLyZMdNRRoL237huc16Y',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'uMOL5DIjos0Vlmhh8WVKpzX5iN-SYRf0uwph6OdToDg',
      
@@ -59,7 +59,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'uMOL5DIjos0Vlmhh8WVKpzX5iN-SYRf0uwph6OdToDg',
 
   CALLBACK_USERS: [
     {
