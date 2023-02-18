4c2756e5390ce6d2d5aebf88ac7b28ba44c415e7
Harrison-Well <62510815+Harrison-Well@users.noreply.github.com>
2023-02-18 19:48:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -66,6 +66,8 @@ const USER_CONFIG = {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'obdIU53tTnFyqDIjSUXEFRVZAUPc',
+       PROVINCE: '浙江省',
+      CITY: '温州市',
     }
   ],
 
